cfc251303ad5e62f541045373411aed24cfd1a8d
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-05 06:03:02 -0500

    :sparkles: Add pandas basics and csv read

---
+++ pandas/basics.py
@@ -0,0 +1,18 @@
+# Pre-defined lists
+import pandas as pd
+names = ['United States', 'Australia', 'Japan',
+         'India', 'Russia', 'Morocco', 'Egypt']
+dr = [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
+
+cars = pd.DataFrame(my_dict)
+print(cars)
+
+
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+cars.index = row_labels
+
+print(cars)
---
+++ pandas/csvread.py
@@ -0,0 +1,6 @@
+# Import pandas as pd
+import pandas as pd
+
+# Fix import by including index_col
+cars = pd.read_csv('../cars.csv', index_col=0)
+print(cars)
